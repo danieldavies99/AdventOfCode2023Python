2be8f04e36c3e57542f9691d6279c703f5dc0b1a
danieldavies99 <danieldavies99@gmail.com>
2023-12-03 11:44:53 +0000

    add loads of comments

--- day3/main.py
+++ day3/main.py
@@ -22,6 +22,21 @@ class Position:
             return self.__key() == other.__key()
         return NotImplemented
 
+# Decode input into a list of a list of characters that will represent
+# the "map" object. Note that if reading directly from map you need to read 
+# in the form map[y][x] (y is switched with x) and larger values of y equate
+# lower positions in the map if you were thinking of it in terms of cartesian 
+# coords
+# 
+# (2, 0):
+# . . @
+# . . .
+# . . .
+#
+# (0, 2)
+# . . .
+# . . .
+# @ . . 
 def decode_input(lines: list[str]) -> list[list[str]]:
     res: list[list[str]] = []
 
@@ -33,12 +48,16 @@ def decode_input(lines: list[str]) -> list[list[str]]:
 
     return res
 
+# Check that a position falls within the bounds of the map
 def position_is_in_map(position: Position, map_width: int, map_height: int) -> bool:
     return position.x >= 0 and position.x < map_width - 1 and position.y >= 0 and position.y < map_height - 1
 
+# get the character at a given x/y position on the map
 def get_symbol_at_point(position: Position, map: list[list[str]]) -> str:
     return map[position.y][position.x]
 
+# get all the neighboring points of a give position, excluding those that would fall
+# outside the bounds of the map
 def get_neighboring_points(position: Position, map: list[list[str]]) -> dict[str, str]:
     def add_positions_to_dict(positions: list[Position]):
         res = {}
@@ -58,6 +77,8 @@ def get_neighboring_points(position: Position, map: list[list[str]]) -> dict[str
         Position(position.x-1, position.y-1), # top left
     ])
 
+# find all numbers in the map and return them as ints along with the positions
+# of each of their individual characters
 def find_all_numbers(map: list[list[str]]) -> list[tuple[int, list[Position]]]:
     res = []
     num_accumulator: str = ''
@@ -81,6 +102,7 @@ def find_all_numbers(map: list[list[str]]) -> list[tuple[int, list[Position]]]:
             pos_accumulator = []
     return res
 
+# given a number and it's positions, determine if it is neighboring a part
 def number_is_neighboring_a_part(number_with_positions: tuple[int, list[Position]], map: list[list[str]]) -> bool:
     for position in number_with_positions[1]:
         neighbors = get_neighboring_points(position, map)
@@ -90,7 +112,9 @@ def number_is_neighboring_a_part(number_with_positions: tuple[int, list[Position
                 return True
     return False
 
+# Part 2 specific functions:
 
+# find the positions of all * symbols in the map
 def find_all_gear_symbols(map: list[list[str]]) -> list[Position]:
     res = []
     for y, line in enumerate(map):
@@ -99,6 +123,7 @@ def find_all_gear_symbols(map: list[list[str]]) -> list[Position]:
                 res.append(Position(x, y))
     return res
 
+# for each gear symbol, return all numbers that it neighbors
 def get_numbers_that_neighbor_position(
         position: Position,
         map: list[list[str]],
@@ -116,6 +141,9 @@ def get_numbers_that_neighbor_position(
             res.append(number_with_position[0])
     return res
 
+# solutions:
+
+# solve part one
 def solve_part_one(map: list[list[str]]) -> int:
     sum = 0
     numbers_with_positions = find_all_numbers(map)
@@ -124,6 +152,7 @@ def solve_part_one(map: list[list[str]]) -> int:
             sum += number_with_position[0]
     return sum
 
+# solve part two
 def solve_part_two(map: list[list[str]]) -> int:
     sum = 0
     numbers_with_positions = find_all_numbers(map)
@@ -135,6 +164,5 @@ def solve_part_two(map: list[list[str]]) -> int:
     return sum
 
 map: list[list[str]] = decode_input(lines)
-
 print("Solution Part One {}".format(solve_part_one(map)))
 print("Solution Part Two {}".format(solve_part_two(map))) 
